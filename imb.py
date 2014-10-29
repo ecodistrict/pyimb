819f45b1a2371110699d9c0443a20518fbc2964b
Rasmus Einarsson <rasmus.einarsson@sp.se>
2014-10-29 07:30:12 +0100

    Add Message and Client

--- imb.py
+++ imb.py
@@ -8,10 +8,12 @@ import logging
 from functools import partial
 from enum import Enum
 
-PORT = 4000
+BYTEORDER = 'little'
 MAGIC_BYTES =  bytes((0x2F, 0x47, 0x61, 0x71, 0x95, 0xAD, 0xC5, 0xFB))
-BYTEORDER = 'big'
-HEADER_LENGTH = 8 #bytes
+END_PAYLOAD_MAGIC_BYTES = int.to_bytes(0x10F13467, 4, BYTEORDER)
+CMD_LENGTH = 4 # bytes
+PAYLOAD_SIZE_LENGTH = 4 # bytes
+HEADER_LENGTH = CMD_LENGTH + PAYLOAD_SIZE_LENGTH
 
 logging.basicConfig(format='[%(levelname)8s] %(message)s', level=logging.DEBUG)
 
@@ -21,13 +23,23 @@ class ClientModes(Enum):
     header = 1
     payload = 2
         
+def decode_header(header):
+    return (
+        int.from_bytes(header[0:CMD_LENGTH], BYTEORDER, signed=True),
+        int.from_bytes(header[CMD_LENGTH:], BYTEORDER, signed=True))
 
 class Message(object):
     """docstring for Message"""
-    def __init__(self, header):
+    def __init__(self, command=None, payload=None):
         super(Message, self).__init__()
-        self.header = header
-        self.payload = None
+        self.command = command
+        self.payload = payload
+
+    @property
+    def header(self):
+        return (
+            self.command.to_bytes(CMD_LENGTH, BYTEORDER, signed=True) + 
+            self.length.to_bytes(PAYLOAD_SIZE_LENGTH, BYTEORDER, signed=True))
 
     @property
     def payload(self):
@@ -35,17 +47,26 @@ class Message(object):
     @payload.setter
     def payload(self, value):
         self._payload = value
+        self.length = len(value) if value else 0
     
     
     
 class Client(asynchat.async_chat):
     """docstring for Client"""
-    def __init__(self, *args, **kwargs):
-        super(Client, self).__init__(*args, **kwargs)
+    def __init__(self, host, port, *args, **kwargs):
+        self.map = {}
+        super(Client, self).__init__(map=self.map, *args, **kwargs)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
         self.set_terminator(MAGIC_BYTES)
         self._ibuffer = []
         self._state = ClientModes.waiting
         self._message = None
+
+        logging.info('Connecting to {0}:{1}'.format(host, port))
+        t = threading.Thread(target=partial(asyncore.loop, map=self.map))
+        t.start()
+
 
     def handle_connect(self):
         logging.info('Connected!')
@@ -60,31 +81,41 @@ class Client(asynchat.async_chat):
         super(Client, self).close()
 
     def found_terminator(self):
-        logging.info('Found terminator.')
+        logging.info('Found terminator in mode {0}.'.format(self._state))
         if self._state is ClientModes.waiting:
             self._ibuffer = []
             self._state = ClientModes.header
             self.set_terminator(HEADER_LENGTH)
         elif self._state is ClientModes.header:
-            self.message = Message(self._ibuffer)
+            print(self._ibuffer, len(self._ibuffer))
+            command, payload_length = decode_header(b''.join(self._ibuffer))
+            logging.info(
+                'Expecting command {0} with payload length {1}.'.format(command, payload_length))
+            self.message = Message(command=command)
             self._ibuffer = []
-            if self.message.length == 0:
+            if payload_length == 0:
                 self.set_terminator(MAGIC_BYTES)
                 self._state = ClientModes.waiting
                 self.handle_message(self.message)
             else:
-                self.set_terminator(self.message.length)
+                self.set_terminator(END_PAYLOAD_MAGIC_BYTES)
                 self._state = ClientModes.payload
-        elif self.state is ClientModes.payload:
-            self.message.payload = self._ibuffer
+        elif self._state is ClientModes.payload:
+            self.message.payload = b''.join(self._ibuffer)
             self.set_terminator(MAGIC_BYTES)
             self._state = ClientModes.waiting
             self.handle_message(self.message)
 
+    def handle_message(self, message):
+        logging.info('Received message: {0} (length {1}): "{2}"'.format(
+            message.command, message.length, message.payload))
 
-        def handle_message(self, msg):
-            print('Message:', msg.header, msg.payload)        
-        
+    def send_message(self, message):
+        self.push(MAGIC_BYTES)
+        self.push(message.header)
+        if message.payload:
+            self.push(message.payload)
+            self.push(END_PAYLOAD_MAGIC_BYTES)        
 
         # try:
         #     while True:
